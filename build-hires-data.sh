6a682f660fb50597399b7adc088ae7b8cdb46b11
Patrick van Staveren <trick@vanstaveren.us>
2018-05-20 23:59:43 +0100

    fix altitude reading

--- build-hires-data.sh
+++ build-hires-data.sh
@@ -6,8 +6,8 @@ set -u
 set -e
 trap 'echo "Error line $LINENO"' ERR
 
-#gngga_lines=$(cat syslog | grep GNGGA | tail -n 2 | cut -d, -f2- | cut -d. -f1) # testing
 gngga_lines=$(cat syslog | grep GNGGA | cut -d, -f2- | cut -d. -f1)
+#gngga_lines=$(cat syslog | grep GNGGA | tail -n 2 | cut -d, -f2- | cut -d. -f1) # testing
 
 echo "timestamp_utc,lat,lon,num_sats,altitude,lm75_temp,bme280_temp,bme280_humidity,bme280_pressure,voltage,current,pi_photo"
 for timestamp in $gngga_lines; do # oh yeah baby, work that bash range magic
@@ -22,7 +22,7 @@ for timestamp in $gngga_lines; do # oh yeah baby, work that bash range magic
     #echo $gps_line
     #echo $sensor_line
     #echo $photo
-    echo -n $(echo $gps_line | cut -d, -f2,3,5,8,12)
+    echo -n $(echo $gps_line | cut -d, -f2,3,5,8,10)
     echo -n ,
     echo -n $(echo $sensor_line | cut -d= -f2 | cut -d, -f1)
     echo -n ,
